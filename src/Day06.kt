a9bbb1663a78ce2269a0bd722508ea89a7d3c717
Adrian Glapiński <adrian.glapinski@rsqtechnologies.com>
2022-12-06 12:33:11 +0100

    Day 06

---
+++ src/Day06.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val input = readInputText("day06_input")
+
+    val result1 = input
+        .windowedSequence(4) { it.toSet() }
+        .indexOfFirst { it.size == 4 } + 4
+    println(result1)
+
+    val result2 = input
+        .windowedSequence(14) { it.toSet() }
+        .indexOfFirst { it.size == 14 } + 14
+    println(result2)
+}
